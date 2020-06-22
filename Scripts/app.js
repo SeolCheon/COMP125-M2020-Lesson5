5f47998b9c1f59a76180b23107992adbac5347b5
SeolCheon <jojo432143@gmail.com>
2020-06-22 14:50:59 -0400

    modified index.htlm to dsplay the proper lesson

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,12 +1,37 @@
 //IIFE - Immediately Invoked Function Expression - anonymous self-executing function-closure
 (function(){
+    //named function   
+   function Start()
+     {
+        let x=5;
+        if(x>="5")
+        {
 
-    function Start()
-    {
-
-        console.log("App Started...");
-    
+            if (confirm("Are you sure?"))
+            {
+                console.log("X is definitely 5");
+            }
+            else
+            {
+                console.log("X is maybe 5???")
+            }
+        }
+        console.log("%cApp Started...", "color:blue; font-size: 24px;");
+        
     }
+
+    
+
+    //declarative format
+    /* let Start =function()
+     {
+        console.log("App Started...");
+     });
+
+    /* let Start = ()=>
+    {
+        console.log("App Started...")
+    } */
     window.addEventListener("load", Start);
 
 })();
